7a9907007a6a2e4e0c308bea84aa7dc30117924c
Calle <cstenson@kth.se>
2016-11-29 09:51:13 +0100

    added an enviroment

---
+++ mediaLab2016/Assets/movementScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementScript : MonoBehaviour {
+
+    public float movementFactor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Move forward
+            transform.position += new Vector3(0,0 , movementFactor); 
+        }else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Move forward
+            transform.position += new Vector3(0, 0, -movementFactor);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Move forward
+            transform.position += new Vector3(-movementFactor, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Move forward
+            transform.position += new Vector3(movementFactor, 0, 0);
+        }
+
+    }
+}
