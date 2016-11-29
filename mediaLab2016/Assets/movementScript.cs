dfe4d44114a9acca2f4b3539063eca97a003905f
Olivia <olivia920628@gmail.com>
2016-11-29 10:10:33 +0100

    change start value of movement factor

--- mediaLab2016/Assets/movementScript.cs
+++ mediaLab2016/Assets/movementScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class movementScript : MonoBehaviour {
 
-    public float movementFactor;
+    public float movementFactor = 0.1;
 
 	// Use this for initialization
 	void Start () {
