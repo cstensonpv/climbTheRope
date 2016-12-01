a5bcb0c2c024b9fef9c93fa7a6770e4bc2011ad8
Calle <cstenson@kth.se>
2016-12-01 16:25:55 +0100

    Added cable and made the movement in steps

--- mediaLab2016/Assets/movementScript.cs
+++ mediaLab2016/Assets/movementScript.cs
@@ -3,34 +3,46 @@ using System.Collections;
 
 public class movementScript : MonoBehaviour {
 
-    public float movementFactor = 0.1;
+    public float movementFactor = 0.1f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private Vector3 endPosition;
+    private Vector3 directionVector;
+
+    // Use this for initialization
+    void Start () {
+        //get goal
+        endPosition = transform.FindChild("endPosition").position;
+        Debug.Log(endPosition);
+        directionVector = (endPosition - transform.position);
+        directionVector = directionVector.normalized;
+
+        Debug.Log(directionVector);
+        
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyUp(KeyCode.UpArrow))
         {
             //Move forward
-            transform.position += new Vector3(0,0 , movementFactor); 
-        }else if (Input.GetKey(KeyCode.DownArrow))
+            transform.position += directionVector * movementFactor; 
+        }else if (Input.GetKeyUp(KeyCode.DownArrow))
         {
             //Move forward
-            transform.position += new Vector3(0, 0, -movementFactor);
+            transform.position -= directionVector * movementFactor;
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            //Move forward
-            transform.position += new Vector3(-movementFactor, 0, 0);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            //Move forward
-            transform.position += new Vector3(movementFactor, 0, 0);
-        }
+        //else if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    //Move forward
+        //    transform.position += new Vector3(-movementFactor, 0, 0);
+        //}
+        //else if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    //Move forward
+        //    transform.position += new Vector3(movementFactor, 0, 0);
+        //}
 
     }
 }
