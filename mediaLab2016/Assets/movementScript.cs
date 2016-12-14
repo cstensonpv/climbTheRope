79d91c9e2f177345a79d00cb5a7b6cfed7527388
Calle <cstenson@kth.se>
2016-12-14 18:53:34 +0100

    fixxed all things except global fog till tomorrow!

--- mediaLab2016/Assets/movementScript.cs
+++ mediaLab2016/Assets/movementScript.cs
@@ -4,50 +4,60 @@ using System.Collections;
 public class movementScript : MonoBehaviour {
 
     public float movementFactor = 0.1f;
+    public float movementSpeed = 1f;
+    public float movementDelta = 0.01f;
 
     private Vector3 endPosition;
     private Vector3 directionVector;
+    private bool isMoving = false;
+    private Vector3 movingFrom;
+    private Vector3 movingTo;
+    private float movingStartTime;
 
     // Use this for initialization
     void Start () {
         //get goal
         endPosition = transform.FindChild("endPosition").position;
-        Debug.Log(endPosition);
         directionVector = (endPosition - transform.position);
         directionVector = directionVector.normalized;
-
-        Debug.Log(directionVector);
-        
-
-
+        directionVector.y = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyUp(KeyCode.UpArrow))
+        if (!isMoving)
         {
-            //Move forward
-            moveForward();
-        }else if (Input.GetKeyUp(KeyCode.DownArrow))
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                //Move forward
+                moveForward();
+            }
+            else if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                //Move backward
+                transform.position -= directionVector * movementFactor;
+            }
+        }else
         {
-            //Move forward
-            transform.position -= directionVector * movementFactor;
-        }
-        //else if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    //Move forward
-        //    transform.position += new Vector3(-movementFactor, 0, 0);
-        //}
-        //else if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    //Move forward
-        //    transform.position += new Vector3(movementFactor, 0, 0);
-        //}
+            //Learp the movement;
+            float distCovered = (Time.time - movingStartTime) * movementSpeed;
+            float fracJourney = distCovered / Vector3.Distance(movingFrom, movingTo);
+            transform.position = Vector3.Lerp(movingFrom, movingTo, fracJourney);
 
+            if(Vector3.Distance(transform.position, movingTo) < movementDelta)
+            {
+                isMoving = false;
+            }
+        }
+        
+       
     }
 
     public void moveForward()
     {
-        transform.position += directionVector * movementFactor;
+        isMoving = true;
+        movingFrom = transform.position;
+        movingTo = transform.position + (directionVector * movementFactor);
+        movingStartTime = Time.time;
     }
 }
